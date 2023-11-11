ba30394cf500696bc287e192ba349f120e6f866a
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2023-11-10 19:32:31 -0800

    Adding travelJournal project to github

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Yo</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,22 @@
+//Update data for Tacoma, Seattle, Spokane
+
+export default [
+    {
+        id: 1,
+        name: "Tacoma",
+        image: "katie-zaferes.png",
+        description: "dope",
+    },
+    {
+        id: 2,
+        name: "Seattle",
+        image: "wedding-photography.png",
+        description: "dope"7,
+    },
+    {
+        id: 3,
+        name: "Spokane",
+        image: "d",
+        description: "dope",
+    }
+] 
