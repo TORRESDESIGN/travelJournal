8d7b7baf5245ad90a37243c6e667b7e3debc4bba
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2023-11-11 17:24:55 -0800

    completed data file, now ready to use.

--- src/data.js
+++ src/data.js
@@ -3,20 +3,20 @@
 export default [
     {
         id: 1,
-        name: "puerto vallarta, MX",
+        name: "Puerto Vallarta, MX",
         image: "puertoV.jpg",
         description: "Puerto Vallarta is defined by the natural charm of its beaches embraced by the Sierra Madre mountains; the essence of its culture, gastronomy and traditions; and the warmth and hospitality of being welcomed like a friend. This magical destination has an authentic Mexican taste and spirit.",
     },
     {
         id: 2,
         name: "Vancouver, BC",
-        image: "wedding-photography.png",
-        description: "dope"7,
+        image: "VancouverBC.jpg",
+        description: "The largest city in British Columbia, Vancouver is a diverse city on the western coast of Canada and a must-see on anyone’s bucket list. Put on a pair of comfy shoes and come hungry! You’ll easily be able to fill your days hopping from district to district in search of good food, great attractions, and impressive scenery. Easy to get around on foot, via public transportation, or by car, you’ll have many options to explore all that awaits you.",
     },
     {
         id: 3,
         name: "Austin, TX",
-        image: "d",
-        description: "dope",
+        image: "austinTX.jpg",
+        description: "With vibrant entertainment, art and culture, inspiring cuisine and stunning outdoor experiences, Austin lets you create a soundtrack all your own. So take a look around, and put the Live Music Capital of the World® on your playlist.",
     }
 ] 
