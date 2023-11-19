fa5d0d753d2433523646d33b979b735edaa5919d
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2023-11-18 19:58:07 -0800

    finished the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './Header.jsx';
 
 function App() {
 
   return (
     <div>
+      <Header />
       <h1>Yo</h1>
     </div>
   )
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<nav className="nav-bar">
+			<i class="fa-solid fa-earth-americas"></i><p>my travel journal</p>
+		</nav>
+	)
+}
+
+
+export default Header;
