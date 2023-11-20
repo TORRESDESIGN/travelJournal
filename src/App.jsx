06b92e74d3b594b4fdf9ebaf7bb30a436bfcf47f
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2023-11-19 20:54:44 -0800

    worked on Card file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './Header.jsx';
+import Card from './Card.jsx';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Card />
       <h1>Yo</h1>
     </div>
   )
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+	return (
+		<div className="card">
+			<img className="card-profile" src="../public/austinTX.jpg"></img>
+			<h1>Austin</h1>
+		</div>
+	)
+}
+
+export default Card;
